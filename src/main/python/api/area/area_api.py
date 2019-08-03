f00147501696442eadf7c23b9d14eab155f4dcce
wg <1134614268@qq.com>
2019-08-03 09:36:41 +0800

    函数变化 组织or地区 变迁 -- 权限变化

---
+++ src/main/python/api/area/area_api.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2019/8/3 9:33
+# @Author  : huangran
+"""
---
+++ src/main/python/api/organization/organization_api.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2019/8/3 9:35
+# @Author  : huangran
+"""
