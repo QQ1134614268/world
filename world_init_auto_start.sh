0ce02fa007788f60851e6031580b6e58cd2cbd72
wg <1134614268@qq.com>
2019-12-29 05:29:46 +0800

    u

--- world_init_auto_start.sh
+++ world_init_auto_start.sh
@@ -5,12 +5,13 @@ cd /app
 git clone  https://gitee.com/biaozhun/world.git
 
 # 同步数据库表结构
-python3 /app/world/src/main/python/manage.py db init
-python3 /app/world/src/main/python/manage.py db migrate
-python3 /app/world/src/main/python/manage.py db upgrade
+cd /app/world/src/main/python
+python3 manage.py db init
+python3 manage.py db migrate
+python3 manage.py db upgrade
 
 # 启动服务
-cd world
+cd /app/world
 pip3 install -r requirements.txt
 nohup python3 /app/world/src/main/python/app.py &
 
--- world_update.sh
+++ world_update.sh
@@ -1,7 +1,12 @@
 cd /app/world
 git pull
 nohup pip3 install -r requirements.txt  &
-python3 /app/world/src/main/python/manage.py db migrate
-python3 /app/world/src/main/python/manage.py db upgrade
+
+cd /app/world/src/main/python
+python3 manage.py db migrate
+python3 manage.py db upgrade
+
+# 启动服务
+cd /app/world
 pkill -f "python3 /app/world/src/main/python/app.py"
 nohup python3 /app/world/src/main/python/app.py &
