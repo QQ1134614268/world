c0ebc59e001e87fcb3a810d0b5c33fa79275548c
wg <1134614268@qq.com>
2020-02-10 13:46:16 +0800

    +收藏模块

--- src/api/customize/BsonToJson.py
+++ src/api/customize/BsonToJson.py
@@ -7,7 +7,8 @@
 # class DateEncoder(json.JSONEncoder):
 #     def default(self, obj):
 #         if isinstance(obj, datetime.datetime):
-#             return obj.strftime('%Y-%m-%d %H:%M:%S')
+#             retu
+#             rn obj.strftime('%Y-%m-%d %H:%M:%S')
 #         elif isinstance(obj, datetime.date):
 #             return obj.strftime("%Y-%m-%d")
 #         else:
