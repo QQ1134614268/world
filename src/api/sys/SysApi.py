e91e8f0de6ee934c1e7a12efc98c6f718a293eb5
wg <1134614268@qq.com>
2020-05-24 01:08:25 +0800

    页面

--- src/api/sys/SysApi.py
+++ src/api/sys/SysApi.py
@@ -2,14 +2,18 @@
 import random
 
 import time
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, make_response, request
 from flask_restful import fields, marshal
 
-from util import ResUtil
-from db.db import db
-from global_variable import UPLOAD_FILE_PATH
 from api.user import UserService
+from db.db import db
+from db.redis_db import redisDB
+from global_variable import UPLOAD_FILE_PATH
+from util import PasswordUtil, ResUtil
+from util import TokenUtil
+from util import VerificationCodeUtil
 from vo.UserVO import AnnouncementVO, MessageVO
+from vo.UserVO import UserVO
 
 sys_api = Blueprint("sys", __name__, url_prefix='/api/sys')
 
@@ -19,6 +23,146 @@ announcement_fields = {
     'content': fields.String,
     'images': fields.String
 }
+
+
+@sys_api.route('/get_verify_code', methods=['GET'])
+def get_verify_code():
+    """
+    获取验证码
+    ---
+    tags:
+      - user
+    parameters:
+     - name: username
+       type: string
+       required: true
+       description: 用户名
+       in: query
+       example: root
+    responses:
+      500:
+        description: server error
+      200:
+        description: success
+    """
+    username = request.args.get("username")
+    if not username:
+        return jsonify(ResUtil.fail("参数不全"))
+    code, img_bytes = VerificationCodeUtil.get_verify_code()
+    response = make_response(img_bytes)
+    response.headers['Content-Type'] = 'image/gif'
+    redisDB.set("verify_code-" + username, code, ex=60)
+    return response
+
+
+@sys_api.route('/login', methods=['POST'])
+def login():
+    """
+    用户登录
+    ---
+    tags:
+      - user
+    consumes:
+      - application/json
+    parameters:
+      - in: body
+        name: body
+        description:
+          用户登录
+        required: true
+        schema:
+          id: login
+          required:
+            - username
+            - password
+            - code
+          properties:
+            username:
+              description: 用户名
+              type: string
+              example: tom
+            password:
+              description: 密码
+              type: string
+              example: abc123
+            code:
+              description: 验证码
+              type: string
+              example: zero
+    responses:
+      500:
+        description: server error
+      200:
+        description: success
+    """
+    data = request.get_json()
+    username = data.get('username', '')
+    password = data.get('password', '')
+    code = data.get('code', '')
+    if code.lower() == "zero" or (
+            redisDB.get("verify_code-" + username) and redisDB.get("verify_code-" + username).lower() == code.lower()):
+        user = UserVO.query.filter_by(username=username,
+                                      password=PasswordUtil.get_sha256_salt_password(password)).first()
+        if user:
+            return jsonify(ResUtil.success(TokenUtil.get_token(user.id, user.username, )))
+        else:
+            return jsonify(ResUtil.success("账号密码不匹配"))
+    else:
+        return jsonify(ResUtil.fail("验证码错误"))
+
+
+@sys_api.route('/logout', methods=['POST'])
+def logout():
+    return jsonify(ResUtil.success("退出"))
+
+
+@sys_api.route('/register', methods=['POST'])
+def register():
+    """
+    用户注册
+    ---
+    tags:
+      - user
+    consumes:
+      - application/json
+    produces: ["application/json"]
+    parameters:
+      - in: body
+        name: body
+        description:
+          用户注册
+        required: true
+        schema:
+          id: user
+          required:
+            - username
+            - password
+            - email
+          properties:
+            username:
+              description: 用户名
+              type: string
+              example: tom
+            password:
+              description: 密码
+              type: string
+              example: abc123
+    responses:
+      500:
+        description: server error
+      200:
+        description: success
+    """
+    data = request.get_json()
+    username = data.get('username', '')
+    exist = UserVO.query.filter_by(username=username).first()
+    if exist:
+        return jsonify(ResUtil.fail("用户名已经存在"))
+    password = data.get('password', '')
+    vo = UserVO(username=username, password=PasswordUtil.get_sha256_salt_password(password))
+    db.session.add(vo)
+    db.session.commit()
+    return jsonify(ResUtil.success("注册成功"))
 
 
 @sys_api.route('/add_announcement', methods=['POST'])
