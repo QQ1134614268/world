35efa0e5eb277ef970e183587daba83a5d004716
wg <1134614268@qq.com>
2020-10-31 19:28:19 +0800

    web加载慢

--- test/__init__.py
+++
@@ -1,4 +0,0 @@
-
-import logging  # 引入logging模块
-logging.basicConfig(level=logging.NOTSET)  # 设置日志级别
-logging.debug(u"如果设置了日志级别为NOTSET,那么这里可以采取debug、info的级别的内容也可以显示在控制台上了")
---
+++ test/test2.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""
+@Time: 2020/10/19
+@Description: 
+"""
+import logging  # 引入logging模块
+
+logging.basicConfig(level=logging.NOTSET)  # 设置日志级别
+logging.debug(u"如果设置了日志级别为NOTSET,那么这里可以采取debug、info的级别的内容也可以显示在控制台上了")
