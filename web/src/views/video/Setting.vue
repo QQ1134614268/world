48260b5fe2a23b761d957cb2d350acb51614777d
wg <1134614268@qq.com>
2022-01-26 00:20:32 +0800

    u

--- web/src/views/video/Setting.vue
+++ web/src/views/video/Setting.vue
@@ -1,23 +1,28 @@
 <template>
-  <div>
-    <el-form ref="form" :model="form" label-width="8rem">
-      <el-form-item label="签名">
-        <el-input v-model="form.describe"></el-input>
+  <div >
+    <el-form ref="form" :model="form" label-width="8rem" :rules="rules" style="padding: 1rem">
+      <el-form-item label="头像">
+        <el-upload style="width: 10%;height: 10%" :show-file-list="false" :action="FileApi" :on-success="uploadAvatar">
+          <img v-if="avatar" :src="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon">点击上传</i>
+        </el-upload>
       </el-form-item>
-      <el-form-item label="手机号">
-        <el-input v-model="form.phone"></el-input>
+      <el-form-item label="手机号" prop="phone" :required="true">
+        <el-input v-model="form.phone" style="width: 16rem"></el-input>
+      </el-form-item>
+      <el-form-item label="签名">
+        <el-input v-model="form.describe" style="width: 16rem"></el-input>
       </el-form-item>
       <el-form-item label="微信号">
-        <el-input v-model="form.wechat_number"></el-input>
+        <el-input v-model="form.wechat_number" style="width: 16rem"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">保存</el-button>
-        <el-button type="primary" @click="onCancel">取消</el-button>
+        <el-button type="primary" @click="save">保存</el-button>
       </el-form-item>
     </el-form>
     <el-dialog title="图片剪裁" :visible.sync="dialogVisible" append-to-body>
       <div class="cropper-content">
-        <div class="cropper" style="text-align:center">
+        <div class="cropper p_c_test_border" style="text-align:center">
           <VueCropper
               ref="cropper"
               :img="option.img"
@@ -36,7 +41,7 @@
         </div>
       </div>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false;">取 消</el-button>
+        <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="finish" :loading="loading">确认</el-button>
       </div>
     </el-dialog>
@@ -44,16 +49,97 @@
 </template>
 
 <script>
+
+import jwt_decode from "jwt-decode";
+import {FileApi, VideoUserApi} from "@/api/api";
+
 export default {
-  name: "Setting",
+  name: "video_user",
   data() {
     return {
-      form: {}
+      avatar:"",
+      user_id: this.$route.query.user_id,
+      form: {},
+      FileApi: FileApi,
+      rules: {
+        phone: [{required: true, message: '手机号不能为空', trigger: 'blur'}],
+      },
+      dialogVisible: false,
+      // 裁剪组件的基础配置option
+      option: {
+        img: '', // 裁剪图片的地址
+        autoCropWidth: 150,
+        autoCropHeight: 150,
+        canMove: true,
+        canMoveBox: true, // 截图框能否拖动
+        info: true, // 裁剪框的大小信息
+        canScale: false, // 图片是否允许滚轮缩放
+        autoCrop: true, // 是否默认生成截图框
+
+        fixedBox: true, // 固定截图框大小 不允许改变
+        // fixed: true, // 是否开启截图框宽高固定比例
+        // fixedNumber: [7, 5], // 截图框的宽高比例
+        full: false, // 是否输出原图比例的截图
+
+        original: false, // 上传图片按照原始比例渲染
+        // centerBox: false, // 截图框是否被限制在图片里面
+        // infoTrue: true, // true 为展示真实输出图片宽高 false 展示看到的截图框宽高,
+        outputType: 'png', // 裁剪生成图片的格式
+        outputSize: 1, // 裁剪生成图片的质量
+      },
+      loading: false
     }
+  },
+
+  methods: {
+    async finish() {
+      let file_url = process.env.VUE_APP_BASE_URL + '/api/file/FileApi2'
+      let formData = new FormData();
+      this.$refs.cropper.getCropBlob(async (data) => {
+        let img = window.URL.createObjectURL(data)
+        this.model = true;
+        this.modelSrc = img;
+        formData.append("file", data, "cropper.png");
+        let result = await this.$postForm(file_url, formData)
+        if (result.data.code) {
+          this.dialogVisible = false
+          this.form.avatar = result.data.data
+        } else {
+          this.$message("失败!")
+        }
+      })
+    },
+    uploadAvatar(res, file, fileList) {
+      this.avatar=res.data
+      this.form.avatar = res.data
+    },
+    async init() {
+      let user = jwt_decode(localStorage.getItem("token"))
+      let result = await this.$get2(VideoUserApi, this.user_id)
+      if (result.data.code == 1) {
+        this.form = result.data.data
+      } else {
+        this.$message('');
+      }
+    },
+    async save() {
+      let result = await this.$putJson2(VideoUserApi, this.form.id, this.form)
+      if (result.data.code == 1) {
+        this.$message('修改成功!');
+      } else {
+        this.$message('');
+      }
+    },
+  },
+  created() {
+    this.init()
   }
 }
 </script>
 
 <style scoped>
-
+.cropper {
+  width: auto;
+  height: 300px;
+}
 </style>
--- web/src/views/video/userspace.vue
+++ web/src/views/video/userspace.vue
@@ -1,18 +1,17 @@
 <template>
   <div style="display:flex; align-items:center ; flex-direction: column; ">
-    <div>
+    <div class="block">
       <router-link to="Setting">编辑</router-link>
     </div>
     <el-avatar :src="form.avatar" :key="form.avatar"></el-avatar>
     <div> 签名: {{ form.describe }}</div>
     <div> 手机号: {{ form.phone }}</div>
     <div> 微信号: {{ form.wechat_number }}</div>
-
   </div>
 </template>
 
 <script>
-import {FileApi, UserApi} from "@/api/api";
+import {UserApi} from "@/api/api";
 
 export default {
   name: "video_user",
@@ -24,83 +23,10 @@ export default {
         phone: [{required: true, message: '手机号不能为空', trigger: 'blur'}],
       },
       dialogVisible: false,
-      // 裁剪组件的基础配置option
-      option: {
-        img: '', // 裁剪图片的地址
-        autoCropWidth: 150,
-        autoCropHeight: 150,
-        canMove: true,
-        canMoveBox: true, // 截图框能否拖动
-        info: true, // 裁剪框的大小信息
-        canScale: false, // 图片是否允许滚轮缩放
-        autoCrop: true, // 是否默认生成截图框
-
-        fixedBox: true, // 固定截图框大小 不允许改变
-        // fixed: true, // 是否开启截图框宽高固定比例
-        // fixedNumber: [7, 5], // 截图框的宽高比例
-        full: false, // 是否输出原图比例的截图
-
-        original: false, // 上传图片按照原始比例渲染
-        // centerBox: false, // 截图框是否被限制在图片里面
-        // infoTrue: true, // true 为展示真实输出图片宽高 false 展示看到的截图框宽高,
-        outputType: 'png', // 裁剪生成图片的格式
-        outputSize: 1, // 裁剪生成图片的质量
-      },
-      loading: false
     }
   },
 
   methods: {
-    // 上传按钮   限制图片大小
-    changeUpload(file, fileList) {
-      let url = URL.createObjectURL(file.raw)
-
-      const isLt5M = file.size / 1024 / 1024 < 5
-      if (!isLt5M) {
-        this.$message.error('上传文件大小不能超过 5MB!')
-        return false
-      }
-      this.fileinfo = file
-      // 上传成功后将图片地址赋值给裁剪框显示图片
-      this.$nextTick(() => {
-        console.log(file)
-        this.option.img = url;
-        // this.option.img = file.url
-        this.dialogVisible = true
-      })
-    },
-    // 点击裁剪，这一步是可以拿到处理后的地址
-    async finish() {
-      let formData = new FormData();
-      this.$refs.cropper.getCropBlob(async (data) => {
-        let img = window.URL.createObjectURL(data)
-        this.model = true;
-        this.modelSrc = img;
-        formData.append("file", data, "cropper.png");
-        let result = await this.$postForm(FileApi, formData)
-        if (result.data.code) {
-          this.dialogVisible = false
-          this.form.avatar = result.data.data
-        } else {
-          this.$message("失败!")
-        }
-      })
-    },
-    handleChange(file, fileList) {
-      this.fileList = fileList.slice(-3);
-    },
-    uploadAvatar(res, file, fileList) {
-      this.form.avatar = res.data
-    },
-    uploadLicense(res, file, fileList) {
-      this.form.business_license = res.data
-    },
-    uploadBrand(res, file, fileList) {
-      this.form.brand = res.data
-    },
-    uploadResume(res, file, fileList) {
-      this.form.resume = res.data
-    },
     async init() {
       let result = await this.$get2(UserApi, this.user_id)
       if (result.data.code == 1) {
@@ -117,9 +43,6 @@ export default {
         this.$message('');
       }
     },
-    async edit() {
-
-    }
   },
   created() {
     this.init()
