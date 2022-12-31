6f92f08604e1536771417b6f026a09ea4aab2959
wg <1134614268@qq.com>
2022-12-31 21:25:08 +0800

    u

--- src/config/log_conf.py
+++ src/config/log_conf.py
@@ -3,7 +3,8 @@
 @Time: 2022/12/25
 @Description:
 """
+from config.conf import world_env
 from util.log_util import create_logger
 
 # logger = create_logger(log_dir=LOG_DIR)
-logger = create_logger()
+logger = create_logger(world_env.log_dir)
